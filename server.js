789c0076ec5dc4fadb15f6f069cffef47353c427
Yıldız Mamak <yldzmamak@gmail.com>
2018-12-06 21:11:53 +0300

    repository init edildi

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var app = express(); // function olarak kullandık
+var PORT = 3000;
+//localhost:3000
+/*
+app.get("/",function(req,res){
+    res.send("merhaba express!!!");
+})
+*/
+
+var middleleware ={
+    requireAuthentication : function(req, res, next){
+        console.log("özel root girildi!!");
+        next();
+    }, 
+    logger : function(req,res,next){
+        console.log(req.method + " " + req.originalUrl);
+        next();
+    }
+}
+
+app.use(middleleware.logger);
+
+app.get("/hakkimda",middleleware.requireAuthentication, function(req,res){
+    res.send("Hakkımda sayfası");
+})
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT , function(){
+    console.log("Express server " + PORT + "nolu portta çalışıyor...");
+});
